eaa107a4fa3fd194d17f3b8e7524c23e0dea761c
mateusvicentee <130100882+mateusvicentee@users.noreply.github.com>
2023-08-03 18:43:29 -0300

    subi os aquivos

---
+++ clickMenu.js
@@ -0,0 +1,9 @@
+function clickMenu(){
+    if(itens.style.display == 'block'){
+        itens.style.display = 'none'
+    } else {
+        itens.style.display = 'block'
+        itens.style.height = '1px'
+        itens.style.padding = '2px'
+    }
+}
